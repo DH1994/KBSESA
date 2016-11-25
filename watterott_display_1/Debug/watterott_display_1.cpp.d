573293a40e07fbbc19024ec08b5925c00b07a053
RickdeGier <rickdegier@outlook.com>
2016-11-25 10:18:25 +0100

    gameField
    working display for generating any map by using an .h and .cpp file.

---
+++ watterott_display_1/Debug/watterott_display_1.cpp.d
@@ -0,0 +1,19 @@
+C:\Users\Rick\AppData\Local\Temp\VMicroBuilds\watterott_display_1\uno\watterott_display_1.cpp.o: \
+ C:\Users\Rick\AppData\Local\Temp\VMicroBuilds\watterott_display_1\uno\watterott_display_1.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Users\Rick\AppData\Local\Temp\VMicroBuilds\watterott_display_1\uno\GameField.h \
+ C:\Users\Rick\Documents\Arduino\libraries\MI0283QT9/MI0283QT9.h \
+ C:\Users\Rick\Documents\Arduino\libraries\GraphicsLib/GraphicsLib.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Users\Rick\Documents\Arduino\libraries\GraphicsLib/fonts.h \
+ C:\Users\Rick\Documents\Arduino\libraries\GraphicsLib/BMPheader.h
