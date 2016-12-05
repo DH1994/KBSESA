886c8fbd00b355091f8a8ee8278987607373e113
dhergaarden <dennis@hergaarden.nl>
2016-12-05 15:51:02 +0100

    Main code

--- Code met pointers/KBSESA1_game_wNC/KBSESA1_game_wNC.ino
+++ Code met pointers/KBSESA1_game_wNC/KBSESA1_game_wNC.ino
@@ -7,6 +7,8 @@
 #include <Wire.h>
 #include "NunchukLibrary.h"
 #include "Player.h"
+#include "Menu.h"
+#include "OptionMenu.h"
 
 #define ADDRESS 0x52
 #define SIZE 24									//is the amount of pixels of on block the game has 9 (y) by 11 (x) blocks and is 216 by 264 px.
@@ -14,6 +16,8 @@
 #define OFFSETY 13
 #define DEBUGON
 #define DEBUG
+
+uint8_t gameStatus;
 
 uint8_t level1[9][11] ={
   {3,0,0,0,0,0,0,0,0,0,0},
@@ -42,15 +46,52 @@ int main(void){
 	lcd = new MI0283QT9();
 	lcd->begin();
 	NC = new NunchukLibrary();
-	playerNC = new Player(NC, MP);
-	gameField = new GameField(lcd, MP, playerNC);
-	while(1){
-		NC->ANupdate();
-		if(playerNC->updatePlayer()){
-			gameField->updateGameField_pl_nc();
+	
+	while(1)
+	{
+		if(gameStatus == 0)
+		{
+			Menu* menu = new Menu(lcd, NC);
+			while(1)
+			{
+				menu->Update();
+				if(menu->getStatus() != 0)
+				{
+					gameStatus = menu->getStatus();
+					break;
+				}
+			}
+			delete menu;
+		}
+				
+		if(gameStatus == 1)
+		{
+			playerNC = new Player(NC, MP);
+			gameField = new GameField(lcd, MP, playerNC);
+			while(1){
+				NC->ANupdate();
+				if(playerNC->updatePlayer()){
+					gameField->updateGameField_pl_nc();
+				}
+			}
+			return 0;	
+		}
+
+		if(gameStatus == 2)
+		{
+			OptionMenu* optMenu = new OptionMenu(lcd, NC);
+			while(1)
+			{
+				optMenu->Update();
+				if(optMenu->getStatus() != 2)
+				{
+					gameStatus = optMenu->getStatus();
+					break;
+				}
+			}
+			delete optMenu;
 		}
 	}
-	return 0;
 }
 /*
 ISR(TIMER2_COMPA_vect)        // interrupt service routine
