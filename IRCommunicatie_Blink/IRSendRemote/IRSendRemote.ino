38b9315b3ee439aaeb43d23ca9c0cbc90284befc
Rumsie <rutgerhakvoort@gmail.com>
2016-12-05 13:27:11 +0100

    Infrarood Communicatie 1
    IR library voor het versturen en ontvangen van afstandsbediening signalen.
    File voor het verzenden van een signaal.
    File voor het ontvangen van een signaal en er voor zorgen dat een LED gaat knipperen.

---
+++ IRCommunicatie_Blink/IRSendRemote/IRSendRemote.ino
@@ -0,0 +1,28 @@
+/*
+  Benodigdheden voor het verzenden:
+  Infrarood LED     x1;
+  Draad             x2;
+  Weerstand 330 Ohm x1;
+
+  LED:
+  Cathode sluit je aan op de ground. (Korte poot)
+  Anode sluit je via de weerstand aan op pin 5(Lange poot). Dit kan je in de library aanpassen.
+*/
+
+#include <IRremote.h>
+
+int main(void)
+{
+  IRsend irsend;
+
+  while(1)
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      // Aan/uit knop van een SONY afstandsbediening wordt hier verstuurd.
+      irsend.sendSony(0xa90, 12);
+      _delay_ms(40);
+    }
+  }
+}
+
