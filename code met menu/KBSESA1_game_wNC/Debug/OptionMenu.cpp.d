9a4a3430d7f72a75c324d80c3e26f67aa6745259
dhergaarden <dennis@hergaarden.nl>
2016-12-06 10:19:50 +0100

    sprint 2 opgeruimd

--- code met menu/KBSESA1_game_wNC/Debug/OptionMenu.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\D.Hergaarden\AppData\Local\Temp\VMicroBuilds\KBSESA1_game_wNC\uno\OptionMenu.cpp.o: \
- C:\Users\D.Hergaarden\AppData\Local\Temp\VMicroBuilds\KBSESA1_game_wNC\uno\OptionMenu.cpp
